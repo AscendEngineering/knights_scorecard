6a3b0bc2bea34808ce6e74e875fef93347e88cd1
Andrew Wilkins <wilkinsaf@gmail.com>
2019-06-29 19:22:59 -0500

    fix bug for serving static files

--- scorecard/urls.py
+++ scorecard/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 from .views import *
 
 urlpatterns = [
     path('', mainPage, name="mainPage"),
     path('knight/', knightsPage, name="knightsPage"),
     path('metrics/', getMetrics, name='getMetrics')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
