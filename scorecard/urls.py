7b33bae2930b7afb7197a57088fd55449995defa
Andrew Wilkins <wilkinsaf@gmail.com>
2019-10-20 13:15:23 -0500

    fix redirect

--- scorecard/urls.py
+++ scorecard/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     url(r'^login/$', splashPage, name='login'),
     url(r'^logout/$', logout, name='logout'),  
     url('access_denied/',access_denied,name='access_denied'),
-    url('scorecard/',scorecard,name='scorecard') 
+    url('scorecard/',scorecard,name='scorecard'),
+   # url('webhooks/',webhook,name='webhook')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
